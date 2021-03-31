65fb1971b9dc796c22311a1999f7ef9c1fef0abf
AndrewKostin <andrewkostin255@ya.ru>
2021-04-01 02:40:03 +0700

    smth

--- help.asm
+++ help.asm
@@ -1,10 +1,20 @@
-asect 0x00
+asect 0x10
+ldi r0, row1
+ldi r1, 123
+st r0, r1
+inc r0
+ldi r1, 13
+st r0, r1
+
+ldi r0, row2
+ldi r1, 35
+st r0, r1
 
 
 halt
-array: dc row1, row2, row3
-
-row1: ds 3
-row2: ds 3
-row3: ds 3
+asect 0x00 	
+	row1: ds 3
+	row2: ds 3
+	row3: ds 3
+	#array: dc row1, row2, row3
 end
