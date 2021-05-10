8ec49d27787176ecf68b836641f9efb3f0b12ca2
AndrewKostin <andrewkostin255@ya.ru>
2021-05-11 05:55:55 +0700

    new version

--- help.asm
+++ help.asm
@@ -1,7 +1,13 @@
 asect 0x00
-# r0, 0x00
-#ldi r1, 0b10010001
-#st r0, r1
+ldi r0, 0b11110011
+ldi r1, 0b10100001
+st r0, r1
+
+ldi r1, 0b10100110
+st r0, r1
+
+ldi r1, 0b10011011
+st r0, r1
 
 #ldi r0, 0x00
 #ldi r1, 0b10100110
